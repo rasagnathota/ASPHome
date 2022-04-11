9b59d7b0b05a01abf4cf3a58c4eb02b36989e3e5
Hp <Hp@DESKTOP-DT9N1AM>
2022-04-11 22:58:37 +0530

    Add project files.

---
+++ ASPHome/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ASPHome.MvcApplication" Language="C#" %>
---
+++ ASPHome/Migrations/202204111223317_Create.cs
@@ -0,0 +1,28 @@
+﻿namespace ASPHome.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Create : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Documents", "PanCard", c => c.Binary());
+            AlterColumn("dbo.Documents", "VoterId", c => c.Binary());
+            AlterColumn("dbo.Documents", "SalarySlip", c => c.Binary());
+            AlterColumn("dbo.Documents", "LOA", c => c.Binary());
+            AlterColumn("dbo.Documents", "NOCFromBuilder", c => c.Binary());
+            AlterColumn("dbo.Documents", "AgreementToSale", c => c.Binary());
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Documents", "AgreementToSale", c => c.Binary(nullable: false));
+            AlterColumn("dbo.Documents", "NOCFromBuilder", c => c.Binary(nullable: false));
+            AlterColumn("dbo.Documents", "LOA", c => c.Binary(nullable: false));
+            AlterColumn("dbo.Documents", "SalarySlip", c => c.Binary(nullable: false));
+            AlterColumn("dbo.Documents", "VoterId", c => c.Binary(nullable: false));
+            AlterColumn("dbo.Documents", "PanCard", c => c.Binary(nullable: false));
+        }
+    }
+}
